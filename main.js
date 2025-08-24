c296cbb902d8e1c6c7e961125c87ddb199e911e6
Michael Mantecon <mmantec@wgu.edu>
2025-08-24 10:44:39 -0700

    Create option ot scale amount of div squares There was no logic in order to change the amount of div squares would be in the dimension of the etch a sketch. Add a button with logic that changes this.

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
-// Create divs within the container div
-const container = document.getElementById("container");
-
-for (let i = 0; i < 256; i++) {
+// Function to create a new div
+function createNewDiv() {
     const newDiv = document.createElement("div");
     newDiv.classList.add("grid-div");
     container.appendChild(newDiv);
 }
 
+// Function to color pixels
+function colorPixels() {
+    const pixelToColor = document.querySelectorAll(".grid-div");
+    pixelToColor.forEach(pixel => {
+        pixel.addEventListener("mouseover", function() {
+            pixel.style.backgroundColor = "#272727";
+        })
+        pixel.addEventListener("mouseout", function() {
+            pixel.style.backgroundColor = "#272727";
+        })
+    })
+}
+
+
+// Create divs within the container div
+const container = document.getElementById("container");
+
+for (let i = 0; i < 256; i++) {
+    createNewDiv();
+}
+
 // color each "pixel" of the etch-a-sketch with lasting effect
-const pixelToColor = document.querySelectorAll(".grid-div");
-pixelToColor.forEach(pixel => {
-    pixel.addEventListener("mouseover", function() {
-        pixel.style.backgroundColor = "#272727";
+colorPixels();
+
+//button changes the number of pixels 
+document.getElementById("button").addEventListener("click",
+    function() {
+        let pixelWidth = prompt("How many pixels wide should the etch-a-sketch be? Enter an answer between 1 and 100!");
+        // Removes current etch-a-sketch grid
+        const gridToRemove = document.querySelectorAll(".grid-div");
+        gridToRemove.forEach(pixel => {
+            pixel.remove();
+        })
+        // Create new etch-a-sketch grid in its place
+        let pixelWidthInt = parseInt(pixelWidth, 10);
+        pixelsToCreate = pixelWidthInt * pixelWidthInt;
+        for (let i = 0; i < pixelsToCreate; i++) {
+            createNewDiv();
+        }
+        // add pixel coloring to grid
+        colorPixels();
+        // Modify flex basis of new grid
+        const changeFlex = document.querySelectorAll(".grid-div");
+        changeFlex.forEach(pixel => {
+            pixel.style.setProperty("flex-basis", `calc(100% / ${pixelWidthInt})`);        })
     })
-    pixel.addEventListener("mouseout", function() {
-        pixel.style.backgroundColor = "#272727";
-    })
-})
